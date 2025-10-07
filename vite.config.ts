6aa8c058fc8f154fdec601dd5689d46a45ba2dae
erkobridee <erko.bridee@gmail.com>
2025-10-07 16:28:59 +0200

    update(github actions): deploy gh pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { name } from "./package.json";
+
+const nodeEnv = `${process.env.NODE_ENV}`;
+
+console.log(`package name: ${name}`);
+console.log(`environment: ${nodeEnv}`);
+
+const isProduction = `${nodeEnv}` === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
+  ...(isProduction ? { base: `/${name}/` } : { build: { sourcemap: true } }),
+
   plugins: [svelte(), tsconfigPaths()],
 });
